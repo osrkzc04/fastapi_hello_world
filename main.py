18859c72eb92080a4cec9fc6c8406bb2fb77b1dd
Oscar Gualoto <opgualoto@puce.edu.ec>
2026-05-22 09:22:01 -0500

    Primer deploy FastAPI

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API funcionando correctamente"}
+
+@app.get("/saludo/{nombre}")
+def saludar(nombre: str):
+    return {"mensaje": f"Hola {nombre}"}
